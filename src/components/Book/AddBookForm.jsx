9674954ac285c8208b27ea404a5197db0d66e96a
Carlos Vivas <carlosmuerto@gmail.com>
2022-09-20 13:40:52 -0400

    highlight Import Redux useDispatch

--- src/components/Book/AddBookForm.jsx
+++ src/components/Book/AddBookForm.jsx
@@ -1,14 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* props-no-spreading disabled because is quired for react-hook-form */
 import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
-import * as bookActions from '../../redux/books/books';
 
 const AddBookForm = () => {
   const { register, handleSubmit } = useForm();
-  const dispatch = useDispatch();
 
-  const onSubmit = (data) => { dispatch(bookActions.addBook(data)); };
+  // eslint-disable-next-line no-console
+  const onSubmit = (data) => { console.log(data); };
   return (
     <div className="add-book-form">
       <form onSubmit={handleSubmit(onSubmit)}>
