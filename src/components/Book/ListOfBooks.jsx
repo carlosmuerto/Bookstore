3b2ae283ca540fea5014f64ede2b5771739a9416
Carlos Vivas <carlosmuerto@gmail.com>
2022-09-20 13:39:14 -0400

    highlight Use useSelector in listOfBooks

--- src/components/Book/ListOfBooks.jsx
+++ src/components/Book/ListOfBooks.jsx
@@ -1,8 +1,7 @@
-import { useSelector } from 'react-redux';
 import BookComponent from './Book';
 
 const ListOfBooks = () => {
-  const books = useSelector((store) => store.books.books);
+  const books = [];
   return (
     <div className="books-lisk">
       {books.map((book) => (<BookComponent key={`books-lisk-card-${book.id}`} book={book} />))}
