1e7ca4244a8b3a9b2a5c89b73907039c45b4740d
Carlos Vivas <carlosmuerto@gmail.com>
2022-09-20 13:39:20 -0400

    Revert "highlight Use useSelector in listOfBooks"
    This reverts commit 3b2ae283ca540fea5014f64ede2b5771739a9416.

--- src/components/Book/ListOfBooks.jsx
+++ src/components/Book/ListOfBooks.jsx
@@ -1,7 +1,8 @@
+import { useSelector } from 'react-redux';
 import BookComponent from './Book';
 
 const ListOfBooks = () => {
-  const books = [];
+  const books = useSelector((store) => store.books.books);
   return (
     <div className="books-lisk">
       {books.map((book) => (<BookComponent key={`books-lisk-card-${book.id}`} book={book} />))}
