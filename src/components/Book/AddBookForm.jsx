2a648a0ab00f87a5e779aa5b9b27fe9abd860257
Carlos Vivas <carlosmuerto@gmail.com>
2022-09-22 11:10:20 -0400

    Add Validation

--- src/components/Book/AddBookForm.jsx
+++ src/components/Book/AddBookForm.jsx
@@ -2,24 +2,54 @@
 /* props-no-spreading disabled because is quired for react-hook-form */
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { addBook } from '../../redux/books/books';
 
+const schema = yup.object({
+  title: yup.string().required(),
+  author: yup.string().required(),
+  category: yup.string().required(),
+}).required();
+
 const AddBookForm = () => {
-  const { register, handleSubmit, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
   const dispatch = useDispatch();
 
+  // eslint-disable-next-line no-console
+  console.log(errors);
   const onSubmit = (data) => {
-    dispatch(addBook(data));
     reset();
+    dispatch(addBook(data));
   };
+
   return (
     <div className="add-book-form">
       <form onSubmit={handleSubmit(onSubmit)}>
         <input {...register('title')} placeholder="title" />
         <input {...register('author')} placeholder="author" />
-        <input {...register('category')} placeholder="category" />
+        <select {...register('category')}>
+          <option value="">Categoty...</option>
+          <option value="Fiction">Fiction</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Action">Action</option>
+          <option value="Romance">Romance</option>
+          <option value="Comedy">Comedy</option>
+          <option value="Education">Education</option>
+        </select>
         <button type="submit">add Book</button>
       </form>
+      <p>{errors.title?.message}</p>
+      <p>{errors.author?.message}</p>
+      <p>{errors.category?.message}</p>
     </div>
   );
 };
